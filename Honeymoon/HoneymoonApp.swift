02a6c00b492bb9120585e11de6187cdd8e97350d
Metin ATALAY <metin.atalay@architecht.com>
2022-04-23 17:35:14 +0300

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Metin Atalay on 23.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
